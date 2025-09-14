fad2785d741893544565cc5fb924ac2a8c3c1289
ElGranPejeRey <erickbarriosarias30@gmail.com>
2025-09-14 19:17:16 -0300

    cambio

--- js/menu.js
+++ js/menu.js
@@ -4,3 +4,10 @@ let menu2 = document.getElementById('menu2')
 hamburguesa.addEventListener("click", () => {
     menu2.classList.toggle('active')
 })
+
+let links = menu2.querySelectorAll("a");
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menu2.classList.remove("active");
+    });
+});
