bbfd70a87c4a370506715684f78e58c1f61123a6
ElGranPejeRey <erickbarriosarias30@gmail.com>
2025-09-16 14:38:51 -0300

    were

--- js/pedidos.js
+++ js/pedidos.js
@@ -4,7 +4,7 @@ document.getElementById("pedidoForm").addEventListener("submit", function(e){
       let nombre = document.getElementById("nombre").value;
       let telefono = document.getElementById("telefono").value;
       let direccion = document.getElementById("direccion").value;
-      let pedido = document.getElementById("pedido").value;
+      let pedido = document.getElementById("pedidos").value;
 
       let numero = "56993641249"; 
 
